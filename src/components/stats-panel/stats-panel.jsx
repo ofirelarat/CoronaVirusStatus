44d1786f8489189543c5df84a396dd97f1b1ecab
ofirelarat <96ofir11@gmail.com>
2021-01-11 21:36:07 +0200

    add comma to numbers

--- src/components/stats-panel/stats-panel.jsx
+++ src/components/stats-panel/stats-panel.jsx
@@ -3,25 +3,38 @@ import './stats-panel.css';
 
 function StatsPanel({title,generalCoronaStats}) {
     console.log(generalCoronaStats)
+
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
     return (
       <div className="stats-panel">
         <div className="stats-title">{title}</div>
         <div className="stats-container">
             <Container className="stat-container">
                 <div className="title">New Confirmed</div>
-                <div className="value">{generalCoronaStats.NewConfirmed}</div>
+                <div className="value">
+                    {numberWithCommas(generalCoronaStats.NewConfirmed)}
+                </div>
             </Container>
             <Container className="stat-container">
                 <div className="title">Total Confirmed</div>
-                <div className="value">{generalCoronaStats.TotalConfirmed}</div>
+                <div className="value">
+                    {numberWithCommas(generalCoronaStats.TotalConfirmed)}
+                </div>
             </Container>
             <Container className="stat-container">
                 <div className="title">New Deaths</div>
-                <div className="value">{generalCoronaStats.NewDeaths}</div>
+                <div className="value">
+                    {numberWithCommas(generalCoronaStats.NewDeaths)}
+                </div>
             </Container>
             <Container className="stat-container">
                 <div className="title">Total Deaths</div>
-                <div className="value">{generalCoronaStats.TotalDeaths}</div>
+                <div className="value">
+                    {numberWithCommas(generalCoronaStats.TotalDeaths)}
+                </div>
             </Container>
         </div>
       </div>
